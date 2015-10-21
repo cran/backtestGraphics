5321bd481394e055e4b2711584bb528084b3cfcd
Miller Zijie Zhu <zijie.miller.zhu@gmail.com>
2015-10-21 10:01:42 +0000

    version 0.1.6

--- inst/doc/backtestGraphics.R
+++ inst/doc/backtestGraphics.R
@@ -1,5 +1,5 @@
 ### R code from vignette source 'backtestGraphics.Rnw'
-### Encoding: UTF-8
+### Encoding: ISO8859-1
 
 ###################################################
 ### code chunk number 1: backtestGraphics.Rnw:93-103
